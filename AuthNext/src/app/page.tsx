6d7cb4d9b245dd0404d0827fdefc23a5cb5148e7
Abhishek1380 <undeabhishek707@gmail.com>
2025-07-21 23:13:30 +0530

    Setup: Next.js + TypeScript + Tailwind + Broken Tailwind problem solved

---
+++ AuthNext/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-slate-100">
+      <h1 className="text-xl font-bold text-blue-600">
+        ✅ Tailwind is working!
+      </h1>
+    </main>
+  );
+}
