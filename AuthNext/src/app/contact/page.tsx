e0c1002e4b32061dd475f68bc30211a3316d87a1
Abhishek1380 <undeabhishek707@gmail.com>
2025-09-12 21:53:28 +0530

    Fresh setup is made.

--- AuthNext/src/app/contact/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function ContactPage() {
-    return (
-        <main className="p-6">
-            <h1 className="text-3xl font-semibold">Contact Us</h1>
-            <p className="mt-4 text-gray-600">Feel free to reach out anytime.</p>
-        </main>
-    );
-}
--- AuthNext/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function HomePage() {
-  return (
-    <main className="p-6">
-      <h1 className="text-4xl font-bold">Welcome to My Startup 🚀</h1>
-      <p className="mt-4 text-lg text-gray-700">This is the home page.</p>
-    </main>
-  );
-}
